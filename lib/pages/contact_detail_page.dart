55b6f5f72589583d69af8b5cd65ce54e88ea7d39
Teerasej <teerasej@gmail.com>
2021-03-10 21:26:13 +0700

    Get argument as data object from named route

--- lib/pages/contact_detail_page.dart
+++ lib/pages/contact_detail_page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:nextflow_flutter_2_contact_app/user_data.dart';
 
 class ContactDetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final Result contactResult = ModalRoute.of(context).settings.arguments;
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Detail'),
